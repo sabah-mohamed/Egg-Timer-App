a5c026385fddf223841a0627086c0d9c0b36d458
Sabah Mohamed <sabah_mohamed23@outlook.com>
2017-04-13 19:44:44 +0200

    Tmer Android Mobile Application
    Application that you can set for a certain timing then alerts you when
    this time is over

---
+++ app/src/main/java/com/sabahmohamed/eggtimer/MainActivity.java
@@ -0,0 +1,91 @@
+package com.sabahmohamed.eggtimer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar timerSeekBar;
+    Button controllerButton;
+    TextView timerTextView;
+    Boolean counterIsActive = false;
+    CountDownTimer countDownTimer;
+
+    public  void resetTimer(){
+
+        timerTextView.setText("0:00");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
+        controllerButton.setText("GO!");
+        timerSeekBar.setEnabled(true);
+        counterIsActive = false;
+    }
+
+    public  void updateTimer(int secondsLeft){
+        int minutes =(int) secondsLeft /60;
+        int seconds = secondsLeft - minutes*60;
+        String secondString = Integer.toString(seconds);
+        if(seconds <= 9){
+            secondString = "0" + secondString;
+        }
+        timerTextView.setText(Integer.toString(minutes) + ":" +secondString );
+
+    }
+
+    public  void controlTimer(View view){
+        if(counterIsActive == false) {
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerButton.setText("Stop!");
+            countDownTimer =  new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    resetTimer();
+                    MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mPlayer.start();
+                }
+            }.start();
+        }
+        else{
+            resetTimer();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+         timerTextView = (TextView) findViewById(R.id.timerTextView);
+        controllerButton = (Button) findViewById(R.id.controllerButton);
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
